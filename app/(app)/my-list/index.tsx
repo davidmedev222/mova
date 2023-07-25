e899c7a0e950973df86ccc10da2e518112911488
davidmedev222 <davidmedev@gmail.com>
2023-07-25 20:07:54 -0300

    feat(ui): add UI of the my list page

--- app/(app)/my-list/index.tsx
+++ app/(app)/my-list/index.tsx
@@ -1,7 +1,13 @@
-import { Text } from 'react-native'
+import { ScrollView } from 'react-native'
+import { MovieList, TagButtonGroup } from '../../../components'
 
 function MyListScreen() {
-  return <Text>My List Screen</Text>
+  return (
+    <ScrollView className='px-2' contentContainerStyle={{ gap: 16, paddingVertical: 8 }}>
+      <TagButtonGroup row />
+      <MovieList />
+    </ScrollView>
+  )
 }
 
 export default MyListScreen
