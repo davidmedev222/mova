6c239e9ca3bec9fcc95aa7173568d655622f234d
davidmedev222 <davidmedev@gmail.com>
2023-07-28 17:44:03 -0300

    feat(ui): add UI of the delete movie modal

--- app/(app)/download/index.tsx
+++ app/(app)/download/index.tsx
@@ -1,16 +1,30 @@
+import { BottomSheetModal } from '@gorhom/bottom-sheet'
+import { useCallback, useRef } from 'react'
 import { ScrollView } from 'react-native-gesture-handler'
-import { MovieDownloadCard } from '../../../components'
+import { Modal, MovieDeleteModal, MovieDownloadCard } from '../../../components'
 
 function DownloadScreen() {
+  const MovieDeleteModalRef = useRef<BottomSheetModal>(null)
+
+  const handleOpenModal = useCallback(() => {
+    MovieDeleteModalRef.current?.present()
+  }, [])
+  const handleCloseModal = useCallback(() => {
+    MovieDeleteModalRef.current?.dismiss()
+  }, [])
+
   return (
     <ScrollView className='px-2' contentContainerStyle={{ gap: 12, paddingVertical: 8 }}>
-      <MovieDownloadCard />
-      <MovieDownloadCard />
-      <MovieDownloadCard />
-      <MovieDownloadCard />
-      <MovieDownloadCard />
-      <MovieDownloadCard />
-      <MovieDownloadCard />
+      <MovieDownloadCard hasDeleteIcon onPressDelete={handleOpenModal} />
+      <MovieDownloadCard hasDeleteIcon onPressDelete={handleOpenModal} />
+      <MovieDownloadCard hasDeleteIcon onPressDelete={handleOpenModal} />
+      <MovieDownloadCard hasDeleteIcon onPressDelete={handleOpenModal} />
+      <MovieDownloadCard hasDeleteIcon onPressDelete={handleOpenModal} />
+      <MovieDownloadCard hasDeleteIcon onPressDelete={handleOpenModal} />
+      <MovieDownloadCard hasDeleteIcon onPressDelete={handleOpenModal} />
+      <Modal ref={MovieDeleteModalRef} minPoint='50%' maxPoint='50%'>
+        <MovieDeleteModal onPressCancel={handleCloseModal} />
+      </Modal>
     </ScrollView>
   )
 }
