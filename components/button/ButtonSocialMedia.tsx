551fdbe12364269534277ffe288f3b8793e41b5e
demattei-teo <teitodematteio@gmail.com>
2023-08-01 12:24:56 -0300

    refactor(component): change static component to reusable

---
+++ components/button/ButtonSocialMedia.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx'
+import { Image, ImageSourcePropType, Pressable, Text } from 'react-native'
+
+interface Props {
+  image: ImageSourcePropType
+  label?: string | undefined
+}
+
+export default function ButtonSocialMedia({ image, label }: Props) {
+  const classes = {
+    button: clsx(
+      'flex-row items-center justify-center space-x-4 rounded-2xl border border-gray-300 bg-gray-200 py-6',
+      label === undefined && 'px-7 py-4'
+    ),
+    icon: clsx('h-6 w-6', label === undefined && 'h-10 w-10'),
+    label: clsx('text-base')
+  }
+
+  return (
+    <Pressable className={classes.button}>
+      <Image className={classes.icon} source={image} />
+      {label !== undefined && <Text className={classes.label}>{label}</Text>}
+    </Pressable>
+  )
+}
